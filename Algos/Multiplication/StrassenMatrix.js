11d63f81054cb4bea228217eb475872229ece4cf
wenxuan27 <wenxuan27@outlook.com>
2020-02-28 15:22:31 -0500

    finished printMatrix

--- Algos/Multiplication/StrassenMatrix.js
+++ Algos/Multiplication/StrassenMatrix.js
@@ -1,5 +1,17 @@
-function strassMult() {
-  
+import * as printMatrix from './printMatrix.js'
+function strassMult(A, B) {
+  M1 = (A[1][1] + A[2][2]) * (B[1][1] + B[2][2])
+  M2 = (A[2][1] + A[2][2]) * B[1][1]
+  M3 = A[1][1] * (B[1][2] - B[2][2])
+  M4 = A[2][2] * (B[2][1] - B[1][1])
+  M5 = (A[1][1] + A[1][2]) * B[2][2]
+  M6 = (A[2][1] - A[1][1]) * (B[1][1] + B[1][2])
+  M7 = (A[1][2] - A[2][2]) * (B[2][1] + B[2][2])
+
+  C[1][1] = M1 + M4 - M5 + M7
+  C[1][2] = M3 + M5
+  C[2][1] = M2 + M4
+  C[2][2] = M1 - M2 + M3 + M6
+  return C
 }
 
-strassMult()
---
+++ Algos/Multiplication/printMatrix.js
@@ -0,0 +1,40 @@
+// M is the matrix
+// n is the number of rows
+// m is the number of columns
+export function printM2d(M, n, m) {
+  string = ''
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      string += M[i][j]
+      // console.log('string is |' + string + '|')
+      if (!(j == m - 1)) {
+        string += ' | '
+      }
+    }
+    if (!(i == n - 1)) {
+      string += '\n'
+      let count = 0
+      for (let k = 0; k < 2 * m + 1; k++) {
+        string += '-'
+        count++
+      }
+
+      string += '\n'
+    }
+  }
+  console.log(string)
+}
+
+// let A = [[], []]
+
+// for (let i = 0; i < 2; i++){
+//   for (let j = 0; j < 2; j++){
+//     A[i][j]
+//   }
+// }
+
+let A = [
+  [1, 2],
+  [3, 4],
+]
+printM2d(A, 2, 2)
