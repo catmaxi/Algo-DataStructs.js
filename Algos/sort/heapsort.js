95cd91b34e63ff09fdfbc6cc9ebe82e17c52fffd
wenxuan27 <wenxuan27@outlook.com>
2020-03-01 00:30:05 -0500

    added process.hrtime

---
+++ Algos/sort/heapsort.js
@@ -0,0 +1,10 @@
+mergesort(arr, l, r){
+  let i, j, k; 
+  let n1 = m - l + 1 
+  let n2 = r - m 
+  
+}
+
+merge(arr, l, m, r){
+
+}
---
+++ Algos/sort/mergesort.js
@@ -0,0 +1,11 @@
+function mergesort(arr, l, r) {
+  let i, j, k
+  let n1 = m - l + 1
+  let n2 = r - m
+  let L, R
+
+  for (i = 0; i < n1; i++) L[i] = arr[l + i]
+  for (j = 0; j < n2; j++) R[j] = arr[m + 1 + j]
+}
+
+function merge(arr, l, m, r) {}
