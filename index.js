258b5c58c4cbac20f7b2109b7979002756c0f3a3
wenxuan27 <wenxuan27@outlook.com>
2020-03-01 01:35:30 -0500

    algos

--- index.js
+++ index.js
@@ -3,5 +3,13 @@
 // Multiplication functions
 exports.printM = require('./algos/Multiplication/printMatrix.js')
 exports.StrassenMatrix = require('./algos/Multiplication/StrassenMatrix.js')
-exports.squareExpMatrix = require('./algos/Multiplication/squareExpMatrix.js')
+exports.squareExpMatrix = require('./algos/multiplication/squareExpMatrix.js')
 exports.squareExp = require('./algos/Multiplication/squareExp.js')
+
+
+// Sort functions
+exports.bubblesort = require('./algos/sort/bubblesort.js')
+exports.bubblesort = require('./algos/sort/bubblesort.js')
+exports.bubblesort = require('./algos/sort/bubblesort.js')
+exports.bubblesort = require('./algos/sort/bubblesort.js')
+exports.bubblesort = require('./algos/sort/bubblesort.js')
